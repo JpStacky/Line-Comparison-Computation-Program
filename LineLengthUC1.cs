fea149a63299ca6db80210c7e8f5043d236ef0c5
Jp <jyotiprakashpc@outlook.com>
2022-08-05 16:27:54 +0530

    To get coordinates and compute line length

---
+++ LineLengthUC1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public static class LineLengthUC1
+    {
+        static int x1, x2, y1, y2;
+        public static void getPointCoordinate()
+        {
+        //Taking coordinates input for 2 points
+        //Input coordinates of first point
+            Console.WriteLine("Enetr Co-ordinate for First Point.");
+            Console.WriteLine("x1 = ");
+            x1 = Convert.ToInt32(Console.Read());
+            Console.WriteLine("y1 = ");
+            y1 = Convert.ToInt32(Console.Read());
+        //Input coordinates of second point
+            Console.WriteLine("Enetr Co-ordinate for Second Point.");
+            Console.WriteLine("x2 = ");
+            x2 = Convert.ToInt32(Console.Read());
+            Console.WriteLine("y2 = ");
+            y2 = Convert.ToInt32(Console.Read());
+            calculateLineLength();
+        }
+        static void calculateLineLength()
+        {
+            //calculate Length of the line
+            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("Length of the line " + length);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            //Calling method that would take coordinate as input and compute
+            LineLengthUC1.getPointCoordinate();
         }
     }
 }
